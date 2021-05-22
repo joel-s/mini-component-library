5a6f761163c2f8ec762330bd0e96ecb939d4b9b6
Joel Sullivan <jsullivan@morsecorp.com>
2021-05-21 21:52:50 -0400

    Revised solution to the Progress Bar exercise after looking at the teacher's solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components';
 
@@ -9,55 +8,63 @@ const SIZES = {
   small: {
     height: 8,
     radius: 4,
-    inset: 0,
+    padding: 0,
   },
   medium: {
     height: 12,
     radius: 4,
-    inset: 0,
+    padding: 0,
   },
   large: {
-    height: 24,
-    radius: 8,
-    inset: 4,
+    height: 16,  // bar height
+    radius: 8,   // radius of outer wrapper
+    padding: 4,  // padding inside wrapper
   },
 };
 
 const ProgressBar = ({ value, size }) => {
-  const { height, radius, inset } = SIZES[size];
-  let rightRadius = (value > 99) ? 4 * (value - 99) : 0;
-  const barHeight = height - (2 * inset);
-  const maxBarLength = 370 - (2 * inset);
+  const { height, radius, padding } = SIZES[size];
   return (
-    <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" height={height} radius={radius}>
-      <Bar
-        length={value * (maxBarLength/100)}
-        height={barHeight}
-        leftRadius={radius}
-        rightRadius={rightRadius}
-        inset={inset}
-      />
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--radius': radius + 'px',
+        '--padding': padding + 'px',
+      }}
+    >
+      <OverflowHider>
+        <Bar
+          style={{
+            '--height': height + 'px',
+            '--value': value + '%',
+          }}
+        />
+      </OverflowHider>
       <VisuallyHidden>{value}</VisuallyHidden>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  height: ${p => p.height}px;
-  width: 370px;
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
-  border-radius: ${p => p.radius}px;
+  padding: var(--padding);
+  border-radius: var(--radius);
+`;
+
+const OverflowHider = styled.div`
+  border-radius: 4px;
+  /* Trim the corners of the bar to fit within the OverflowHider */
+  overflow: hidden;
 `;
 
 const Bar = styled.div`
-  position: relative;
-  top: ${p => p.inset}px;
-  left: ${p => p.inset}px;
-  height:${p => p.height}px;
-  width: ${p => p.length}px;
   background-color: ${COLORS.primary};
-  border-radius: 4px ${p => p.rightRadius}px ${p => p.rightRadius}px 4px;
+  height: var(--height);
+  width: var(--value);
 `;
 
 export default ProgressBar;
